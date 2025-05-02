b72e262424cab1f781d7b9171a02200602267175
Michael Nobles <Mnobles33@gmail.com>
2025-05-01 19:34:03 -0700

    4th commit

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ class Counter {
    */
   constructor(initialNumber) {
     // ✨ initialize whatever properties are needed
+    this.count = initialNumber
   }
 
   /**
@@ -71,6 +72,10 @@ class Counter {
    */
   countDown() {
     // ✨ implement
+    if (this.count > 0) {
+      return this.count--
+    }
+    return this.count
   }
 }
 
